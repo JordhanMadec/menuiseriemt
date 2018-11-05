d0c168acc187de0acd6af722d75dcf903fb03010
Jordhan <jordhan.madec@gmail.com>
2018-11-05 18:46:00 +0100

    Validators for forgot/reset password

--- src/app/login/forgot-password/forgot-password.component.ts
+++ src/app/login/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
+  private forgotPasswordForm: FormGroup;
+  private mailFailed = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.forgotPasswordForm = new FormGroup({
+      'mail': new FormControl('', [
+        Validators.required,
+        Validators.email
+      ])
+    });
   }
 
+  onSubmit() {
+  }
 }
--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+export const passwordMatcher = (control: AbstractControl): {[key: string]: boolean} => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if  (!password) {
+    return null;
+  }
+
+  if (password.value === confirmPassword.value) {
+    return null;
+  }
+
+  return {mismatch: true};
+};
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +23,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  private resetPasswordForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.resetPasswordForm = this.fb.group({
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, {validator: passwordMatcher});
+  }
+
+  onSubmit() {
+  }
 }
+
+
