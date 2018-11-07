4f3ab13d08e7b50848d51bc71e4620a49f191010
Jordhan <jordhan.madec@gmail.com>
2018-11-07 10:12:41 +0100

    Set firebase environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
-import {BrowserModule, HammerGestureConfig} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './home/about/about.component';
@@ -20,6 +21,9 @@ import { NgxPageScrollModule } from 'ngx-page-scroll';
 import { ScrollSpyModule } from 'ngx-scrollspy';
 import { NgsRevealModule } from 'ngx-scrollreveal';
 import { LegalsComponent } from './legals/legals.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 const APP_ROUTES: Routes = [
   {path: '', component: HomeComponent},
@@ -47,10 +51,13 @@ const APP_ROUTES: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgxPageScrollModule,
     RouterModule.forRoot(APP_ROUTES),
+    NgxPageScrollModule,
     ScrollSpyModule.forRoot(),
     NgsRevealModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
