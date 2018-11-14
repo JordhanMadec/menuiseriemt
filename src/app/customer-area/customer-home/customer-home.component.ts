5efcd02f69596e1ffd4468b8b7b9c776e29ffb62
Jordhan <jordhan.madec@gmail.com>
2018-11-14 11:00:05 +0100

    Fetch user + nav buttons

--- src/app/customer-area/customer-home/customer-home.component.ts
+++ src/app/customer-area/customer-home/customer-home.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-customer-area-home',
   templateUrl: './customer-home.component.html',
   styleUrls: ['./customer-home.component.scss']
 })
-export class CustomerHomeComponent implements OnInit {
+export class CustomerHomeComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  public user: User;
+
+  private userSubscription: Subscription;
+
+  constructor(private cd: ChangeDetectorRef, private authService: AuthService) {
   }
 
   ngOnInit() {
+    this.userSubscription = this.authService.currentUser.subscribe(
+      user => {
+        this.user = user;
+
+        this.cd.detectChanges();
+      }
+    );
   }
 
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 }
