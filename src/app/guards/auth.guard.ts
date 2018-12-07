8688c80ff5198e0e3a5c2ff220fe2a0637d1db04
Jordhan <jordhan.madec@gmail.com>
2018-12-07 16:18:19 +0100

    Auth guard redirect admin

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable, NgZone } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { fromPromise } from 'rxjs-compat/observable/fromPromise';
+import { map, switchMap, take } from 'rxjs/operators';
+import { User } from '../models/user';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
@@ -12,9 +14,16 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.authService.isAuthenticated().pipe(
       take(1),
-      map(user => {
+      switchMap(user => {
         if (!user) {
           this.ngZone.run(() => this.router.navigate(['login']));
+        }
+
+        return fromPromise(this.authService.refreshCurrentUser());
+      }),
+      map((user: User) =>{
+        if (user && user.isAdmin) {
+          this.ngZone.run(() => this.router.navigate(['espace-admin']));
         }
 
         return true;
