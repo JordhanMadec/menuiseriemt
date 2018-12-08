942f89e729ff55ebb56808e25e05d89ab0b1a82d
Jordhan <jordhan.madec@gmail.com>
2018-12-08 16:30:45 +0100

    Fetch projects

--- src/app/admin-area/admin-projects/admin-projects.component.ts
+++ src/app/admin-area/admin-projects/admin-projects.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { AdminService } from '../../services/admin.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-admin-projects',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProjectsComponent implements OnInit {
 
-  constructor() { }
+  public projects: Project[];
 
-  ngOnInit() {
+  constructor(private cd: ChangeDetectorRef, private authService: AuthService, private adminService: AdminService) {
   }
 
+  ngOnInit() {
+    this.fetchProjects();
+  }
+
+  fetchProjects() {
+    this.adminService.getAllProjects().then(
+      (projects: Project[]) => {
+        this.projects = projects;
+        this.cd.detectChanges();
+      }
+    );
+  }
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/app';
 import { environment } from '../../environments/environment';
+import { Project } from '../models/project';
 import { User } from '../models/user';
 import { AlertService } from './alert.service';
 import { AuthService } from './auth.service';
@@ -24,7 +25,7 @@ export class AdminService {
         .ref('/users')
         .once('value')
         .then(_users => {
-          const users: User[] = [];
+          const users = [];
 
           _users.forEach(user => {
             users.push(new User(user.val()));
@@ -116,5 +117,37 @@ export class AdminService {
     }
 
     return password;
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // PROJECTS
+
+  getAllProjects(): Promise<Project[]> {
+    return firebase.database()
+      .ref('/projects')
+      .once('value')
+      .then(_projects => {
+        const projects = [];
+
+        _projects.forEach(user => {
+          user.forEach(project => {
+            projects.push(new Project(project.val()));
+          })
+        })
+
+        return projects;
+      }, error => {
+        this.alertService.error('Impossible de récupérer les chantiers');
+        return [];
+      });
   }
 }
