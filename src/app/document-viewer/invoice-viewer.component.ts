fe686c525e11d5168b42504cc4b81e643a7fd699
Jordhan <jordhan.madec@gmail.com>
2018-12-09 20:15:44 +0100

    Display project owner

--- src/app/document-viewer/invoice-viewer.component.ts
+++ src/app/document-viewer/invoice-viewer.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Invoice } from '../models/invoice';
 import { Project } from '../models/project';
+import { User } from '../models/user';
 import { AuthService } from '../services/auth.service';
 import { DatabaseService } from '../services/database.service';
 import { StorageService } from '../services/storage.service';
@@ -20,6 +21,7 @@ export class InvoiceViewerComponent implements OnInit, OnDestroy {
   private isAdminSubscription: Subscription;
 
   public project: Project;
+  public user: User;
 
   public document: Invoice;
   public documentUrl: string;
@@ -46,6 +48,11 @@ export class InvoiceViewerComponent implements OnInit, OnDestroy {
 
         this.storageService.getInvoiceUrl(this.customerId, this.document.fileName).then(url => {
           this.documentUrl = url;
+          this.cd.detectChanges();
+        });
+
+        this.databaseService.getUser(this.customerId).then((user: User) => {
+          this.user = user;
           this.cd.detectChanges();
         });
 
--- src/app/document-viewer/quote-viewer.component.ts
+++ src/app/document-viewer/quote-viewer.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Project } from '../models/project';
 import { Quote } from '../models/quote';
+import { User } from '../models/user';
 import { AuthService } from '../services/auth.service';
 import { DatabaseService } from '../services/database.service';
 import { StorageService } from '../services/storage.service';
@@ -20,6 +21,7 @@ export class QuoteViewerComponent implements OnInit, OnDestroy {
   private isAdminSubscription: Subscription;
 
   public project: Project;
+  public user: User;
 
   public document: Quote;
   public documentUrl: string;
@@ -46,6 +48,11 @@ export class QuoteViewerComponent implements OnInit, OnDestroy {
 
         this.storageService.getQuoteUrl(this.customerId, this.document.fileName).then(url => {
           this.documentUrl = url;
+          this.cd.detectChanges();
+        });
+
+        this.databaseService.getUser(this.customerId).then((user: User) => {
+          this.user = user;
           this.cd.detectChanges();
         });
 
