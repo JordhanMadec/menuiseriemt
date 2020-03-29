4b5c2e969713f48408c82b59083b53aadbf3ec0c
Jordhan <jordhan.madec@gmail.com>
2020-03-29 13:23:55 +0200

    add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCu9Yi8u2ChZUQUU7CDOXmd0_7EpWsy0c4',
+    authDomain: 'menuiseriemt-7b106.firebaseapp.com',
+    databaseURL: 'https://menuiseriemt-7b106.firebaseio.com',
+    projectId: 'menuiseriemt-7b106',
+    storageBucket: 'gs://menuiseriemt-7b106.appspot.com',
+    messagingSenderId: '438492585415',
+  }};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCu9Yi8u2ChZUQUU7CDOXmd0_7EpWsy0c4',
+    authDomain: 'menuiseriemt-7b106.firebaseapp.com',
+    databaseURL: 'https://menuiseriemt-7b106.firebaseio.com',
+    projectId: 'menuiseriemt-7b106',
+    storageBucket: 'gs://menuiseriemt-7b106.appspot.com',
+    messagingSenderId: '438492585415',
+  }
+};
