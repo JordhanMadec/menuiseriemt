a7867c59d0a4036fb84b3878961e7ed839f802fd
Jordhan <jordhan.madec@gmail.com>
2020-11-11 11:37:35 +0100

    Zoom in album background on hover

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,12 +1,12 @@
-import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Observable} from 'rxjs/Observable';
-import {AlbumInfo} from './album-info.model';
-import {Http} from '@angular/http';
-import 'rxjs/add/observable/of';
-import {Subscription} from 'rxjs/Subscription';
-import {catchError, map} from 'rxjs/operators';
-import {of} from 'rxjs/internal/observable/of';
+import { Component, HostListener, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import { AlbumInfo } from "./album-info.model";
+import { Http } from "@angular/http";
+import "rxjs/add/observable/of";
+import { Subscription } from "rxjs/Subscription";
+import { catchError, map } from "rxjs/operators";
+import { of } from "rxjs/internal/observable/of";
 
 declare var $: any;
 
@@ -17,51 +17,56 @@ export enum KEY_CODE {
 }
 
 @Component({
-  selector: 'app-album',
-  templateUrl: './album.component.html',
-  styleUrls: ['./album.component.scss']
+  selector: "app-album",
+  templateUrl: "./album.component.html",
+  styleUrls: ["./album.component.scss"],
 })
 export class AlbumComponent implements OnInit, OnDestroy {
   subscription: Subscription;
 
   album: Observable<AlbumInfo>;
   private mAlbum: AlbumInfo;
-  private numbers: number[];
+  public numbers: number[];
 
   private path: string;
   private fullsScreenId = 0;
 
-  constructor(private router: Router, private route: ActivatedRoute, private http: Http) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private http: Http
+  ) {}
 
   ngOnInit() {
     window.scrollTo(0, 0);
 
-    this.subscription = this.route.params.subscribe(
-      (params: any) => {
-        const albumId = params['id'];
+    this.subscription = this.route.params.subscribe((params: any) => {
+      const albumId = params["id"];
 
-        this.getAlbum(albumId).subscribe(
-          (album: AlbumInfo) => {
-            this.getAllPhotos(album.path);
-            // Get the number of photos to display
-            this.numbers = Array(album.length).fill(0).map((x, i) => i + 1);
-            this.album = of(album);
-            this.mAlbum = album;
-            console.log('GetAlbum SUCCESS: ', album);
-          },
-          error => {
-            console.log('GetAlbum ERROR: ', error);
-          });
-      }
-    );
+      this.getAlbum(albumId).subscribe(
+        (album: AlbumInfo) => {
+          this.getAllPhotos(album.path);
+          // Get the number of photos to display
+          this.numbers = Array(album.length)
+            .fill(0)
+            .map((x, i) => i + 1);
+          this.album = of(album);
+          this.mAlbum = album;
+          console.log("GetAlbum SUCCESS: ", album);
+        },
+        (error) => {
+          console.log("GetAlbum ERROR: ", error);
+        }
+      );
+    });
   }
 
   goToHome() {
-    this.router.navigate([''], {fragment: 'gallery-section'});
+    this.router.navigate([""], { fragment: "gallery-section" });
   }
 
-  private getAlbum(id:  string): Observable<AlbumInfo> {
-    return this.http.get('/assets/gallery-db.json').pipe(
+  private getAlbum(id: string): Observable<AlbumInfo> {
+    return this.http.get("/assets/gallery-db.json").pipe(
       map((res: any) => {
         const albums = res.json();
         for (const album of albums) {
@@ -74,28 +79,28 @@ export class AlbumComponent implements OnInit, OnDestroy {
     );
   }
 
-  private getAllPhotos(path:  string): Observable<any> {
+  private getAllPhotos(path: string): Observable<any> {
     return this.http.get(path).pipe(
       map((res: any) => {
-        console.log('ALL PHOTOS: ', res);
+        console.log("ALL PHOTOS: ", res);
       }),
-      catchError(error => {
-        console.log('ALL PHOTOS ERROR: ', error);
+      catchError((error) => {
+        console.log("ALL PHOTOS ERROR: ", error);
         return of(error);
       })
     );
   }
 
   showFullScreen(id: number) {
-    $('.full-screen-photo-container').fadeIn(500);
+    $(".full-screen-photo-container").fadeIn(500);
 
     if (this.mAlbum) {
       this.fullsScreenId = id;
-      $('#full-screen-photo').attr('src', this.mAlbum.path + '/' + id + '.png');
+      $("#full-screen-photo").attr("src", this.mAlbum.path + "/" + id + ".png");
     }
   }
 
-  @HostListener('window:keyup', ['$event'])
+  @HostListener("window:keyup", ["$event"])
   keyEvent(event: KeyboardEvent) {
     // console.log(event);
 
@@ -113,41 +118,49 @@ export class AlbumComponent implements OnInit, OnDestroy {
   }
 
   hideFullScreen() {
-    $('.full-screen-photo-container').fadeOut(500);
+    $(".full-screen-photo-container").fadeOut(500);
   }
 
   nextPhoto() {
     if (this.mAlbum) {
-      this.fullsScreenId = this.numbers[this.fullsScreenId % this.mAlbum.length];
-      const src = this.mAlbum.path + '/' + this.fullsScreenId + '.png';
+      this.fullsScreenId = this.numbers[
+        this.fullsScreenId % this.mAlbum.length
+      ];
+      const src = this.mAlbum.path + "/" + this.fullsScreenId + ".png";
 
-      $('#full-screen-photo').fadeOut(300, function() {
-        $('#full-screen-photo').attr('src', src);
-      }).fadeIn(300);
+      $("#full-screen-photo")
+        .fadeOut(300, function () {
+          $("#full-screen-photo").attr("src", src);
+        })
+        .fadeIn(300);
     }
   }
 
   previousPhoto() {
     if (this.mAlbum) {
-      this.fullsScreenId = this.numbers[(((this.fullsScreenId - 2) % this.mAlbum.length) + this.mAlbum.length) % this.mAlbum.length];
-      const src = this.mAlbum.path + '/' + this.fullsScreenId + '.png';
+      this.fullsScreenId = this.numbers[
+        (((this.fullsScreenId - 2) % this.mAlbum.length) + this.mAlbum.length) %
+          this.mAlbum.length
+      ];
+      const src = this.mAlbum.path + "/" + this.fullsScreenId + ".png";
 
-      $('#full-screen-photo').fadeOut(300, function() {
-        $('#full-screen-photo').attr('src', src);
-      }).fadeIn(300);
+      $("#full-screen-photo")
+        .fadeOut(300, function () {
+          $("#full-screen-photo").attr("src", src);
+        })
+        .fadeIn(300);
     }
   }
 
   showCommands() {
-    $('#commands').show();
+    $("#commands").show();
   }
 
   hideCommands() {
-    $('#commands').hide();
+    $("#commands").hide();
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
-
 }
